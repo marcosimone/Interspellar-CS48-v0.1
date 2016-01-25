5aba64c373ce7a4d29d5d84a0475652ed43c7e8f
Andrew Meeyers <LittleBreadLoaf@Andrews-MacBook-Pro-2.local>
2016-01-24 16:01:29 -0800

    less dumb walljump

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ from soundboard import soundboard
 class Player:
 	health = 1000
 	velocity=0
+	xvelocity=0
 	x=0
 	def __init__(self, screen, sound, level, player_pos):
 		self.screen=screen
@@ -28,7 +29,7 @@ class Player:
 		body=Rect((self.pos[0]-32,self.pos[1]-64), (64,64))
 		xpos=self.pos[0]
 		ypos=self.pos[1]
-		
+		pygame.draw.rect(self.screen,Color("red"),self.level[0])
 		col_index=body.collidelist(self.level)
 		if self.velocity < -20:
 			self.velocity =-20
@@ -43,17 +44,34 @@ class Player:
 						
 						ypos=plat.top-5
 					self.velocity=15
-			else:
+			#else:
 				#self.velocity=0
-				ypos+=5
+			#	ypos+=5
 				
 			if body.bottom>plat.top-3 and body.top<plat.bottom+3:
 				if fabs(body.right-plat.left)<5:
-					self.velocity-=0.3
+					if self.velocity > 5:
+						self.velocity-=1.3
+					else:
+						self.velocity-=0.25
+					if self.velocity < -8:
+						self.velocity = -8
 					xpos=plat.left-32
+					if inputs[0]:
+						self.velocity = 10
+						self.xvelocity = -8
+						
 				elif fabs(body.left-plat.right)<5:
-					self.velocity-=0.3
+					if self.velocity > 5:
+						self.velocity-=1.3
+					else:
+						self.velocity-=0.25
+					if self.velocity < -8:
+						self.velocity = -8
 					xpos=plat.right+32
+					if inputs[0]:
+						self.velocity = 10
+						self.xvelocity = 8
 		else:
 			if ypos < 720:
 				self.velocity-=.5
@@ -63,6 +81,11 @@ class Player:
 				else:
 					self.velocity=0
 		ypos=ypos-self.velocity
+		xpos=xpos+self.xvelocity
+		if self.xvelocity > 0:
+			self.xvelocity-=0.4
+		if self.xvelocity < 0:
+			self.xvelocity+=0.4
 		if inputs[1]:
 			xpos-=4
 		if inputs[3]:
